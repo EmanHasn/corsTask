d3412ab003c2bebe5c3525b981faa72046e2e7de
Eman Hassan <emanhhsacr@gmail.com>
2023-07-10 16:00:00 +0200

    corsTask

--- model/note.js
+++ model/note.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TaskSchema = new mongoose.Schema(
   {
     title: { type: String, required: true ,  unique: true},
-    decription: { type: String, required: true },
+    description: { type: String, required: true },
     completed: { type: Boolean, required: true, default: false },
     password: { type: String, required: true },
 }
